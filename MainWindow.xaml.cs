4e9a9b471164bf447ef957d8e57685e96264bc5b
Luis barreto <luis.barreto.marin@gmail.com>
2020-01-21 10:01:55 -0400

    Desarrollando interfaz de usuario

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventario_y_Contabilidad
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            if(!!File.Exists("bd.sdf"))
+            {
+                MessageBox.Show("No existe bd.sdf", "Error");
+                Environment.Exit(0);
+            }
+
+            InitializeComponent();
+        }
+
+        private void btnDetalle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCambiarTasa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void imgVender_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Venta venta = new Venta();
+            venta.Owner = this;
+            venta.ShowDialog();
+        }
+    }
+}
---
+++ Venta.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventario_y_Contabilidad
+{
+    /// <summary>
+    /// Lógica de interacción para Venta.xaml
+    /// </summary>
+    public partial class Venta : Window
+    {
+        public Venta()
+        {
+            InitializeComponent();
+            this.txtIdArt.Focus();
+        }
+
+        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void aceptarCompra(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cancelarCompra(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
